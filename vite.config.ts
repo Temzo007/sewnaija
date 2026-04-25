f25103368e8b810278f319858c4ca14bd5acd43c
Akintola Emmanuel <emmanuelakintola007@gmail.com>
2026-04-25 14:34:12 +0100

    Fix formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
       manifest: false,
 
       includeAssets: [
-        "favicon.ico",
+        "favicon.ico",   
         "icon-192.png",
         "icon-512.png"
       ],
